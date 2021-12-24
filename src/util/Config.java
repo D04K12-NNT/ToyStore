4819689b69bb6e45e96df0dbbbb2339ec862b455
NNT9x <42613109+nnt9x@users.noreply.github.com>
2021-12-24 19:46:51 +0700

    Update Config.java

--- src/util/Config.java
+++ src/util/Config.java
@@ -3,6 +3,6 @@ package util;
 final class Config {
 	// Config SQL Server
 	static final String USER = "sa";
-	static final String PASSWORD = "Bkacad@123";
+	static final String PASSWORD = "your_password";
 	static final String DB_NAME = "toy_store";
 }
