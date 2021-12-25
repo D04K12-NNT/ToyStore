48157fb2d06c7bd2ea3c9a8022c343eec96ad4c6
NNT9x <42613109+nnt9x@users.noreply.github.com>
2021-12-25 08:57:57 +0700

    Thêm cấu hình database của các em vào đây

--- src/util/Config.java
+++ src/util/Config.java
@@ -1,7 +1,7 @@
 package util;
 
 final class Config {
-	// Config SQL Server
+	// Thêm cấu hình DB tại đây
 	static final String USER = "sa";
 	static final String PASSWORD = "your_password";
 	static final String DB_NAME = "your_db";
